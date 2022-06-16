e8c3a4942cc4da1ab74dd4e7060f6ce71baeeee5
Jeonghun Lee <2220110021@office.kopo.ac.kr>
2022-06-16 22:35:19 +0900

    indexOf, lastIndexOf

--- src/main/java/practice/p0614.java
+++ src/main/java/practice/p0614.java
@@ -9,7 +9,9 @@ public class p0614 {
 
 
 
-        /** indexOf(), lastIndexOf() ---------- 특정 문자열의 위치를 찾는 메소드 **/
+        /** indexOf(), lastIndexOf() ---------- 특정 문자열의 위치를 찾는 메소드
+         *
+         * 첫번째 나오는 java의 위치와 마지막에 나오는 java 의 위치 **/
  /*       String str = "java시간 java는 너무 재미져!";
         System.out.println("문자열 : " + str);
 
@@ -19,7 +21,9 @@ public class p0614 {
         System.out.println(str.lastIndexOf("java"));*/
 
 
-        /** startsWith(), endsWith() 메소드 ---------- 문자열 처음 또는 끝이 특정 문자열인지 확인하는 메소드 **/
+        /** startsWith(), endsWith() 메소드 ---------- 문자열 처음 또는 끝이 특정 문자열인지 확인하는 메소드
+         *
+         * 입력받은 문자열 처음 시작과 끝을 '()' 로 감싸주기 **/
  /*       Scanner sc = new Scanner(System.in);
         String str;
         System.out.print("문자 입력 : ");
@@ -35,7 +39,9 @@ public class p0614 {
             System.out.printf(")");*/
 
 
-        /** charAt() 메소드 .----- 문자열 위치 o를 $로 바꾸기 **/
+        /** charAt() 문자열 위치 메소드 ----------- 문자열 바꾸기
+         *
+         * 입력받은 문자열 o를 $로 바꾸기 **/
 /*        Scanner sc = new Scanner(System.in);
         String str;
 
@@ -50,7 +56,9 @@ public class p0614 {
         }*/
 
 
-        /** length() 메소드 ---------- 문자열 길이 **/
+        /** length() 메소드  -------------- 문자열 길이 출력하기
+         *
+         * it cookbook 의 문자열 출력하기 **/
  /*       String str = "it cookbook";
         int len;
         len = str.length();
