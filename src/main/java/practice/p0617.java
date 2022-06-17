d781326eafd72d95f7f62f09e7309229f5ec03ab
Jeonghun Lee <2220110021@office.kopo.ac.kr>
2022-06-17 23:33:36 +0900

    지역변수와 전역변수 비교

---
+++ src/main/java/practice/p0617.java
@@ -0,0 +1,15 @@
+package practice;
+
+public class p0617 {
+    static int a = 100;
+
+    static void func1(){
+        int a = 200;
+        System.out.printf("func1()에서 a의 값 ==> %d\n",a);
+    }
+
+    public static void main(String[] args) {
+        func1();
+        System.out.printf("main() 에서 a의 값 ==> %d\n",a);
+    }
+}
