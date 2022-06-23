f371e7ee480c0eee0be215fb7d6dde5e737c8689
Jeonghun Lee <2220110021@office.kopo.ac.kr>
2022-06-23 23:05:52 +0900

    시험 복습ㄴ

---
+++ src/main/java/practice/p0622.java
@@ -0,0 +1,64 @@
+package practice;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class p0622 {
+    public int sum( int num1, int num2){
+        int sum = num1 + num2;
+        return sum;
+/*
+        String log = "111.222.333.44 -- [11/jun/2012:22:12:32 + 0900]\"get";
+
+        String [] ip = log.split(" ");
+        System.out.println(ip[3].substring(1));*/
+
+       /* List<String> pList = new ArrayList<>();
+        pList.add("이협건");
+        pList.add("홍길동");
+        pList.add("고길동");
+        pList.add("둘리");
+
+        pList.forEach(name -> System.out.println("name : " + name));*/
+
+       /* for ( String name : pList){
+            System.out.println(name);
+        }*/
+
+
+       /* List<String> pList = new ArrayList<>();
+        pList.add("홍길동");
+        pList.add("고길동");
+        for (String name : pList){
+            System.out.println(name);
+        }*/
+
+       /* List<Map<String, String>> list = new ArrayList<>();
+
+        Map<String, String> map = new HashMap<>();
+
+        map.put("name", "이협건");
+        map.put("dept", "데이터분석");
+
+        list.add(map);
+
+        map = null;
+
+        map.put("name", "고길동");
+        map.put("dept", "정보보안");
+
+        list.add(map);
+
+        map = null;
+*/
+
+
+/*        List<String>pList = new ArrayList<>();
+        pList.add("홍길동");
+        pList.add("이협건");
+        pList.add("고길동");
+        pList.add("둘리");*/
+    }
+}
