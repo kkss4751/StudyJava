b3ef3d04ee3dd5950ac9b1173baa8833e66ff1ba
Jeonghun Lee <2220110021@office.kopo.ac.kr>
2022-06-27 23:54:46 +0900

    자바의 정석 첫문제

---
+++ src/main/java/practice/p0627.java
@@ -0,0 +1,13 @@
+package practice;
+
+public class p0627 {
+    public static void main(String[] args) {
+        //w자바의 정석
+        int a = 10, b = 20;
+        int tmp;
+
+        tmp = a;
+        a = b;
+        b = a;
+    }
+}
